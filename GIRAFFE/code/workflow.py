771c2d8bda51fefa0b1052e043bea91397bf2719
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-03-14 00:39:40 +0100

    select files iterable

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -5,21 +5,16 @@ import sys
 import nipype
 import nipype.pipeline as pe
 
-import nipype.interfaces.utility as utility
 import nipype.interfaces.io as io
 import nipype.interfaces.fsl as fsl
 import nipype.interfaces.spm as spm
 
-#Basic interface class generates identity mappings
-utility_IdentityInterface = pe.Node(utility.IdentityInterface(fields=["subject", "run"]), name='utility_IdentityInterface')
-utility_IdentityInterface.inputs.subject = ['01', '02', '03']
-utility_IdentityInterface.inputs.run = [1, 2]
-
 #Flexibly collect data from disk to feed into workflows.
-io_SelectFiles = pe.Node(io.SelectFiles(templates={'anatomical':'Subject{subj_id}/Anatomical/anat.nii.gz','functional':'Subject{subj_id}/Functional/rEP3D_Session*.nii'}), name='io_SelectFiles')
+io_SelectFiles = pe.Node(io.SelectFiles(templates={'anatomical':'Subject{subj_id}/Anatomical/anat.nii.gz','functional':'Subject{subj_id}/Functional/rEP3D_Session*.nii','subj_id':['01', '02', '03']}), name='io_SelectFiles')
 io_SelectFiles.inputs.base_directory = '/project/3015003.04/FamTutorial/SubjectData/'
 io_SelectFiles.inputs.anatomical = 'Subject{subj_id}/Anatomical/anat.nii.gz'
 io_SelectFiles.inputs.functional = 'Subject{subj_id}/Functional/rEP3D_Session*.nii'
+io_SelectFiles.iterables = [('subj_id', ['01', '02', '03'])]
 
 #Wraps the executable command ``bet``.
 fsl_BET = pe.Node(interface = fsl.BET(), name='fsl_BET')
@@ -38,7 +33,6 @@ io_DataSink.inputs.base_directory = '/project/3015003.04/FamTutorial/Results/'
 analysisflow = nipype.Workflow('MyWorkflow')
 analysisflow.connect(io_SelectFiles, "functional", spm_Realign, "in_files")
 analysisflow.connect(io_SelectFiles, "anatomical", fsl_BET, "in_file")
-analysisflow.connect(utility_IdentityInterface, "subject", io_SelectFiles, "subj_id")
 analysisflow.connect(fsl_BET, "out_file", fsl_FLIRT, "in_file")
 analysisflow.connect(spm_Realign, "mean_image", fsl_FLIRT, "reference")
 analysisflow.connect(fsl_FLIRT, "out_file", io_DataSink, "registered")
